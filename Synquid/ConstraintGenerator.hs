dd47e07b6a40a7ad639b5f33d5a4df699f594382
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2015-05-26 12:36:46 -0400

    Refactored constraint generator to use more state.

--- Synquid/ConstraintGenerator.hs
+++ Synquid/ConstraintGenerator.hs
@@ -12,6 +12,7 @@ import Data.Set (Set)
 import qualified Data.Map as Map
 import Data.Map (Map)
 import Control.Monad.State
+import Control.Monad.Reader
 import Control.Applicative
 import Control.Lens
 import Control.Monad.Trans.Maybe
@@ -35,12 +36,13 @@ data ConsGenParams = ConsGenParams {
 -- such that a valid solution for the constraints would turn the liquid program into a simple program of type @typ@.
 genConstraints :: ConsGenParams -> QualsGen -> QualsGen -> Environment -> RType -> Template -> ([Clause], QMap, LiquidProgram)
 genConstraints params cq tq env typ templ = if bottomUp params
-                                              then evalState goBottomUp 0
-                                              else evalState goTopDown 0
+                                              then runReader (evalStateT goBottomUp (0, [])) params
+                                              else runReader (evalStateT goTopDown (0, [])) params
   where
     goTopDown :: Generator ([Clause], QMap, LiquidProgram)
     goTopDown = do
-      (p, cs) <- constraintsTopDown env typ templ
+      p <- constraintsTopDown env typ templ
+      cs <- gets snd
       let cs' = concatMap split cs
       let (clauses, qspaces) = partitionEithers $ map (toFormula cq tq) cs'
       let qmap = Map.unions qspaces      
@@ -48,19 +50,20 @@ genConstraints params cq tq env typ templ = if bottomUp params
       
     goBottomUp :: Generator ([Clause], QMap, LiquidProgram)
     goBottomUp = do
-      (p, t, cs_) <- constraintsBottomUp env templ
-      let cs = [Subtype env t typ] ++ cs_
-      let cs' = concatMap split $ cs
+      (p, t) <- constraintsBottomUp env templ
+      addConstraint $ Subtype env t typ
+      cs <- gets snd
+      let cs' = concatMap split cs
       let (clauses, qspaces) = partitionEithers $ map (toFormula cq tq) cs'
       let qmap = Map.unions qspaces      
       debug 1 (text "Typing Constraints" $+$ (vsep $ map pretty cs)) $ return (clauses, qmap, p)
 
 {- Implementation -}
   
-type Generator = State Int
+type Generator = StateT (Int, [Constraint]) (Reader ConsGenParams)
 
 freshId :: String -> Generator String
-freshId prefix = ((prefix ++) . show) <$> state (\i -> (i, i + 1))
+freshId prefix = ((prefix ++) . show) <$> state (\(i, cs) -> (i, (i + 1, cs)))
   
 -- | 'freshRefinements' @t@ : a type with the same shape and value variables as @t@ but fresh unknowns as refinements
 freshRefinements :: RType -> Generator RType
@@ -70,16 +73,19 @@ freshRefinements (ScalarT base _) = do
 freshRefinements (FunctionT x tArg tFun) = do
   liftM3 FunctionT (freshId "_x") (freshRefinements tArg) (freshRefinements tFun)
   
+addConstraint c = modify (\(i, cs) -> (i, c:cs))  
+  
 -- | 'constraintsTopDown' @env t templ@ : a liquid program and typing constraints 
 -- for a program of type @t@ following @templ@ in the typing environment @env@
-constraintsTopDown :: Environment -> RType -> Template -> Generator (LiquidProgram, [Constraint])
+constraintsTopDown :: Environment -> RType -> Template -> Generator LiquidProgram
 constraintsTopDown env t (PSymbol _) = do
   -- t' <- freshRefinements t
   let t' = t
   let symbols = symbolsByShape (shape t') env
   let leafFml = Map.mapWithKey (fmlForSymbol t') symbols
   let disjuncts = map (:[]) $ Map.elems $ Map.mapWithKey (constraintForSymbol t') symbols
-  return (PSymbol leafFml, [WellFormedSymbol disjuncts])
+  addConstraint $ WellFormedSymbol disjuncts
+  return $ PSymbol leafFml
   -- case t of
     -- ScalarT _ _ -> return (PSymbol leafFml, [WellFormedScalar env t', Subtype env t' t])
     -- FunctionT _ _ _ -> return (PSymbol leafFml, [WellFormedSymbol disjuncts, WellFormed (clearSymbols $ clearAssumptions env) t', Subtype env t' t])
@@ -95,19 +101,21 @@ constraintsTopDown env t (PApp funTempl argTempl) = do
   x <- freshId "_x"
   tArg <- freshRefinements $ refine $ sTypeOf argTempl
   let tFun = FunctionT x tArg t
-  (fun, csFun) <- constraintsTopDown env tFun funTempl
-  (arg, csArg) <- constraintsTopDown env tArg argTempl     
-  return (PApp fun arg, csArg ++ csFun ++ [WellFormed (clearRanks env) tArg])
+  fun <- constraintsTopDown env tFun funTempl
+  arg <- constraintsTopDown env tArg argTempl     
+  addConstraint $ WellFormed (clearRanks env) tArg
+  return $ PApp fun arg
 constraintsTopDown env t (PFun _ bodyTempl) = do
   let (FunctionT x tArg tRes) = t
   let env' = addSymbol (Var x) tArg env
-  (pBody, cs) <- constraintsTopDown env' tRes bodyTempl
-  return (PFun x pBody, cs)
+  pBody <- constraintsTopDown env' tRes bodyTempl
+  return $ PFun x pBody
 constraintsTopDown env t (PIf _ thenTempl elseTempl) = do
   cond <- Unknown Map.empty <$> freshId "_u"
-  (pThen, csThen) <- constraintsTopDown (addAssumption cond env) t thenTempl
-  (pElse, csElse) <- constraintsTopDown (addNegAssumption cond env) t elseTempl
-  return (PIf cond pThen pElse, csThen ++ csElse ++ [WellFormedCond env cond])
+  pThen <- constraintsTopDown (addAssumption cond env) t thenTempl
+  pElse <- constraintsTopDown (addNegAssumption cond env) t elseTempl
+  addConstraint $ WellFormedCond env cond
+  return $ PIf cond pThen pElse
 constraintsTopDown env t (PFix _ bodyTemp) = do
   f <- freshId "_f"  
   -- t' <- freshRefinements t
@@ -123,20 +131,25 @@ constraintsTopDown env t (PFix _ bodyTemp) = do
   let tArg' = ScalarT IntT (fml |&| (valueVar |>=| IntLit 0) |&| (valueVar |<| Var m))
   let tArg'' = ScalarT IntT (fml |&| (valueVar |=| Var m))
   let env' = addSymbol (Var f) (FunctionT y tArg' (renameVar x y tRes)) . addRank (Var m) $ env
-  (pBody, cs) <- constraintsTopDown env' (FunctionT x tArg'' tRes) bodyTemp
-  return (PFix f pBody, cs) -- ++ [WellFormed (clearRanks env) t', Subtype env t' t])    
+  pBody <- constraintsTopDown env' (FunctionT x tArg'' tRes) bodyTemp
+  return $ PFix f pBody -- ++ [WellFormed (clearRanks env) t', Subtype env t' t])    
   
 -- | 'constraintsBottomUp' @env templ@ : a liquid program, its type, and typing constraints 
 -- for a program following @templ@ in the typing environment @env@  
-constraintsBottomUp :: Environment -> Template -> Generator (LiquidProgram, RType, [Constraint])
+constraintsBottomUp :: Environment -> Template -> Generator (LiquidProgram, RType)
 constraintsBottomUp env (PSymbol t) = do
   t' <- freshRefinements $ refine t
   let symbols = symbolsByShape t env
   let leafFml = Map.mapWithKey (fmlForSymbol t') symbols
   let disjuncts = map (:[]) $ Map.elems $ Map.mapWithKey (constraintForSymbol t') symbols
   case t of
-    ScalarT _ _ -> return (PSymbol leafFml, t', [WellFormedScalar env t'])
-    FunctionT _ _ _ -> return (PSymbol leafFml, t', [WellFormedSymbol disjuncts, WellFormed (clearSymbols $ clearAssumptions env) t']) -- ToDo: use space union for all symbols as a search space
+    ScalarT _ _ -> do
+      addConstraint $ WellFormedScalar env t'
+      return (PSymbol leafFml, t')
+    FunctionT _ _ _ -> do
+      addConstraint $ WellFormedSymbol disjuncts
+      addConstraint $ WellFormed (clearSymbols $ clearAssumptions env) t'
+      return (PSymbol leafFml, t') -- ToDo: use space union for all symbols as a search space
   where    
     constraintForSymbol t symb symbT = Subtype  (clearSymbols $ clearAssumptions env) (symbolType symb symbT) t
     fmlForSymbol t symb symbT = let         
@@ -145,22 +158,29 @@ constraintsBottomUp env (PSymbol t) = do
     symbolType (Var x) (ScalarT b _) = ScalarT b (varRefinement x)
     symbolType _ t = t
 constraintsBottomUp env (PApp funTempl argTempl) = do
-  (fun, FunctionT x tArg tRes, csFun) <- constraintsBottomUp env funTempl
-  (arg, tArg', csArg) <- constraintsBottomUp env argTempl     
-  return (PApp fun arg, typeConjunction (renameVar valueVarName x tArg') tRes, [Subtype env tArg' tArg] ++ csArg ++ csFun)
+  (fun, FunctionT x tArg tRes) <- constraintsBottomUp env funTempl
+  (arg, tArg') <- constraintsBottomUp env argTempl
+  addConstraint $ Subtype env tArg' tArg
+  return (PApp fun arg, typeConjunction (renameVar valueVarName x tArg') tRes)
 constraintsBottomUp env templ@(PFun _ bodyTempl) = do
   t@(FunctionT x tArg tRes) <- freshRefinements $ refine $ sTypeOf templ
   let env' = addSymbol (Var x) tArg env
-  (pBody, tRes', cs) <- constraintsBottomUp env' bodyTempl
-  return (PFun x pBody, t, [WellFormed env t, Subtype env' tRes' tRes] ++ cs)
+  (pBody, tRes') <- constraintsBottomUp env' bodyTempl
+  addConstraint $ WellFormed env t
+  addConstraint $ Subtype env' tRes' tRes
+  return (PFun x pBody, t)
 constraintsBottomUp env (PIf _ thenTempl elseTempl) = do
   t <- freshRefinements $ refine $ sTypeOf thenTempl
   cond <- Unknown Map.empty <$> freshId "_u"
   let envThen = addAssumption cond env
   let envElse = addNegAssumption cond env
-  (pThen, tThen, csThen) <- constraintsBottomUp envThen thenTempl
-  (pElse, tElse, csElse) <- constraintsBottomUp envElse elseTempl
-  return (PIf cond pThen pElse, t, [WellFormed env t, WellFormedCond env cond, Subtype envThen tThen t, Subtype envElse tElse t] ++ csThen ++ csElse)
+  (pThen, tThen) <- constraintsBottomUp envThen thenTempl
+  (pElse, tElse) <- constraintsBottomUp envElse elseTempl
+  addConstraint $ WellFormed env t
+  addConstraint $ WellFormedCond env cond
+  addConstraint $ Subtype envThen tThen t
+  addConstraint $ Subtype envElse tElse t  
+  return (PIf cond pThen pElse, t)
 constraintsBottomUp env (PFix s bodyTemp) = do
   t@(FunctionT x tArg tRes) <- freshRefinements $ refine s
   f <- freshId "_f"
@@ -170,9 +190,11 @@ constraintsBottomUp env (PFix s bodyTemp) = do
   let tArg' = ScalarT IntT (fml |&| (valueVar |>=| IntLit 0) |&| (valueVar |<| Var m))
   let tArg'' = ScalarT IntT (fml |&| (valueVar |=| Var m))
   let env' = addSymbol (Var f) (FunctionT y tArg' (renameVar x y tRes)) . addRank (Var m) $ env
-  (pBody, t', cs) <- constraintsBottomUp env' bodyTemp
-  return (PFix f pBody, t, [WellFormed env t, Subtype env t' (FunctionT x tArg'' tRes)] ++ cs)  
-    
+  (pBody, t') <- constraintsBottomUp env' bodyTemp
+  addConstraint $ WellFormed env t
+  addConstraint $ Subtype env t' (FunctionT x tArg'' tRes)
+  return (PFix f pBody, t)  
+      
 -- | 'split' @c@ : split typing constraint @c@ that may contain function types into simple constraints (over only scalar types)
 split :: Constraint -> [Constraint]
 split (Subtype env (FunctionT x tArg1 tRes1) (FunctionT y tArg2 tRes2)) =
