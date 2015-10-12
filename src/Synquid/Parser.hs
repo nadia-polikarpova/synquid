1ea377d70c4e2f1f8fe3ec72b6d2b254513ba57e
Severyn Kozak <severyn.kozak@gmail.com>
2015-10-12 13:32:54 -0400

    src/Synquid/Parser.hs: convert hard tabs to soft ones.
    ...to maintain consistency with the rest of the codebase.

--- src/Synquid/Parser.hs
+++ src/Synquid/Parser.hs
@@ -14,8 +14,8 @@ applyParser parser = Parsec.parse parser ""
 
 parse :: String -> TypeSkeleton Formula
 parse str = case Parsec.parse parseType "" str of
-	Right parsed -> parsed
-	Left err -> error "foobar!"
+  Right parsed -> parsed
+  Left err -> error "foobar!"
 
 parseType :: Parser (TypeSkeleton Formula)
 parseType = Parsec.choice [parseScalarType{-, parseFunctionType-}]
@@ -25,40 +25,41 @@ parseScalarType = Parsec.choice [parseScalarRefType, parseScalarUnrefType]
 
 parseScalarUnrefType :: Parser (TypeSkeleton Formula)
 parseScalarUnrefType = do
-	baseType <- parseBaseType
-	return $ ScalarT baseType [] $ BoolLit True
+  baseType <- parseBaseType
+  return $ ScalarT baseType [] $ BoolLit True
 
 parseScalarRefType :: Parser (TypeSkeleton Formula)
 parseScalarRefType = do
-	Parsec.char '{'
-	Parsec.spaces
-	baseType <- parseBaseType
-	Parsec.spaces
-	Parsec.char '|'
-	Parsec.spaces
-	refinement <- parseRefinement
-	Parsec.spaces
-	Parsec.char '}'
-	return $ ScalarT baseType [] refinement
+  Parsec.char '{'
+  Parsec.spaces
+  baseType <- parseBaseType
+  Parsec.spaces
+  Parsec.char '|'
+  Parsec.spaces
+  refinement <- parseRefinement
+  Parsec.spaces
+  Parsec.char '}'
+  return $ ScalarT baseType [] refinement
 
 parseBaseType :: Parser BaseType
 parseBaseType = BoolT <$ Parsec.string "Bool" <|> IntT <$ Parsec.string "Int"
 
 parseRefinement = Expr.buildExpressionParser exprTable (parseTerm <* Parsec.spaces)
-	where
-		exprTable = [
-			[bin "*" (|*|)],
-			[bin "+" (|+|), bin "-" (|-|), bin "/+" (/+/), bin "/*" (/*/), bin "/-" (/-/)],
-			[bin "=" (|=|), bin "/=" (|/=|), bin "<=" (|<=|), bin "<" (|<|), bin ">=" (|>=|), bin ">" (|>|),
-				bin "/<=" (/<=/)],
-			[bin "&" (|&|), bin "|" (|||)],
-			[bin "=>" (|=>|), bin "<=>" (|<=>|)]]
-		bin opStr func =  Expr.Infix parseOpStr Expr.AssocLeft
-			where parseOpStr = Parsec.try $ do
-				Parsec.string opStr
-				Parsec.notFollowedBy $ Parsec.oneOf ">=" -- This is a dirty hack.
-				Parsec.spaces
-				return func
+  where
+    exprTable = [
+      [bin "*" (|*|)],
+      [bin "+" (|+|), bin "-" (|-|), bin "/+" (/+/), bin "/*" (/*/), bin "/-" (/-/)],
+      [bin "=" (|=|), bin "/=" (|/=|), bin "<=" (|<=|), bin "<" (|<|), bin ">=" (|>=|), bin ">" (|>|),
+        bin "/<=" (/<=/)],
+      [bin "&" (|&|), bin "|" (|||)],
+      [bin "=>" (|=>|), bin "<=>" (|<=>|)]]
+    bin opStr func =  Expr.Infix parseOpStr Expr.AssocLeft
+      where
+        parseOpStr = Parsec.try $ do
+          Parsec.string opStr
+          Parsec.notFollowedBy $ Parsec.oneOf ">=" -- This is a dirty hack.
+          Parsec.spaces
+          return func
 
 parseTerm :: Parser Formula
 parseTerm = Parsec.choice [Parsec.between (Parsec.char '(') (Parsec.char ')') parseRefinement, parseBoolLit, parseIntLit]
@@ -71,10 +72,10 @@ parseIntLit = fmap (IntLit . read) $ Parsec.many1 Parsec.digit
 
 parseUnaryOp :: Parser Formula
 parseUnaryOp = do
-	op <- Neg <$ Parsec.char '-' <|> Not <$ Parsec.char '!'
-	refinement <- parseRefinement
-	return $ Unary op refinement
+  op <- Neg <$ Parsec.char '-' <|> Not <$ Parsec.char '!'
+  refinement <- parseRefinement
+  return $ Unary op refinement
 
 parseBinaryOp :: Parser (Formula -> Formula -> Formula)
 parseBinaryOp = fmap Binary $ Parsec.choice [Times <$ Parsec.string "*", Plus <$ Parsec.string "+"]
-	where
+  where
