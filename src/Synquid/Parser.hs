55e4fffa937a43976588effa49f6c45bb0ebbdd7
Severyn Kozak <severyn.kozak@gmail.com>
2015-10-25 12:51:11 -0400

    Extend the parser to support the full Synquid grammar.
    Mostly complete the parser, making it support the full Synquid program
    specification grammar. It's not too well tested and there are some rough
    edges/problems (comments can only be used alongside declarations and not in
    them, etc).

--- src/Synquid/Parser.hs
+++ src/Synquid/Parser.hs
@@ -1,4 +1,4 @@
-{- 
+{-
  - The parser for Synquid's program specification DSL.
  -}
 
@@ -6,28 +6,146 @@ module Synquid.Parser where
 
 import Synquid.Logic
 import Synquid.Program
+
 import qualified Control.Applicative as Applicative
 import Control.Applicative ((<$), (*>), (<*))
+import Control.Monad.Except
+import Control.Monad.Identity
+
 import qualified Text.Parsec as Parsec
 import qualified Text.Parsec.Token as Token
 import qualified Text.Parsec.Expr as Expr
+import qualified Text.Parsec.Char as Char
 import Text.Parsec ((<|>), (<?>))
 
+import Text.Printf
+
 type Parser = Parsec.Parsec String ()
+-- Rschema or skeleton?
+type ProgramAst = [Declaration]
+data ConstructorDef = ConstructorDef Id RSchema-- deriving Show
+data Declaration =
+  TypeDef Id RType | -- | Type name and definition.
+  FuncDef Id RSchema | -- | Function name and signature.
+  DataDef Id [Id] [ConstructorDef] | -- | Datatype name, type parameters, and constructor definitions.
+  MeasureDef Id Id [Sort] Sort | -- | Measure name, input type name, input type parameters, output sort
+  SynthesisGoal Id
+
+-- Quick and dirty Show instances to aid in debugging.
+instance Show Declaration where
+  show (TypeDef id' type') = printf "type %s = %s" id' $ show type'
+  show (FuncDef id' schema) = printf "func %s = %s" id' $ show schema
+  show (DataDef id' typeParams ctors) = printf "data %s %s\n%s" id' (show typeParams) $ unlines $ map ((++) "  " . show) ctors
+  show (MeasureDef id' inType typeParams outSort) = printf "msre %s = %s (%s) -> %s" id' inType (show typeParams) $ show outSort
+  show (SynthesisGoal id') = printf "%s = ??" id'
+
+instance Show ConstructorDef where
+  show (ConstructorDef id' schema) = printf "ctor %s = %s" id' $ show schema
 
 applyParser parser = Parsec.parse parser ""
 
-parse :: String -> TypeSkeleton Formula
-parse str = case Parsec.parse parseType "" str of
-  Right parsed -> parsed
-  Left err -> error "foobar!"
+parse parser str = case Parsec.parse parser "" str of
+  Left err -> Left $ show err
+  Right parsed -> Right parsed
+
+parseProgram :: Parser ProgramAst
+parseProgram = parseCommentOrWs *> Parsec.sepEndBy1 parseDeclaration parseCommentOrWs <* Parsec.eof
+  where
+    parseCommentOrWs = Parsec.many $ (Parsec.space >> Parsec.spaces) <|> parseComment
+    parseComment = do
+      Parsec.string "--"
+      Parsec.manyTill Parsec.anyChar Parsec.endOfLine
+      return ()
+
+parseDeclaration :: Parser Declaration
+parseDeclaration = Parsec.choice [parseTypeDef, parseDataDef, parseMeasureDef, Parsec.try parseSynthesisGoal, parseFuncDef] <?> "declaration"
+
+parseTypeDef :: Parser Declaration
+parseTypeDef = do
+  Parsec.try $ Parsec.string "type"
+  Parsec.spaces
+  typeName <- parseTypeName
+  Parsec.spaces
+  Parsec.char '='
+  Parsec.spaces
+  typeDef <- parseType
+  return $ TypeDef typeName typeDef
+  <?> "type definition"
+
+parseDataDef :: Parser Declaration
+parseDataDef = do
+  Parsec.try $ Parsec.string "data"
+  Parsec.spaces
+  typeName <- parseTypeName
+  Parsec.spaces
+  typeParams <- Parsec.manyTill (parseIdentifier <* Parsec.spaces) $ Parsec.string "where"
+  Parsec.spaces
+  constructors <- (Parsec.many1 $ parseConstructorDef <* Parsec.spaces)
+  return $ DataDef typeName typeParams constructors
+  <?> "data definition"
+
+parseConstructorDef :: Parser ConstructorDef
+parseConstructorDef = do
+  ctorName <- parseTypeName
+  Parsec.spaces
+  Parsec.string "::"
+  Parsec.spaces
+  ctorType <- parseSchemaSkeleton
+  return $ ConstructorDef ctorName ctorType
+  <?> "constructor definition"
+
+parseMeasureDef :: Parser Declaration
+parseMeasureDef = do
+  Parsec.try $ Parsec.string "measure"
+  Parsec.spaces
+  measureName <- parseIdentifier
+  Parsec.spaces
+  Parsec.string "::"
+  Parsec.spaces
+  inTypeName <- parseTypeName
+  Parsec.spaces
+  inTypeParams <- Parsec.many $ parseSort <* Parsec.spaces
+  Parsec.spaces
+  Parsec.string "->"
+  Parsec.spaces
+  outSort <- parseSort
+  return $ MeasureDef measureName inTypeName inTypeParams outSort
+  <?> "measure definition"
+
+parseFuncDef :: Parser Declaration
+parseFuncDef = do
+  funcName <- parseIdentifier
+  Parsec.spaces
+  Parsec.string "::"
+  Parsec.spaces
+  fmap (FuncDef funcName) parseSchemaSkeleton
+  <?> "function definition"
+
+parseSynthesisGoal :: Parser Declaration
+parseSynthesisGoal = do
+  goalId <- parseIdentifier
+  Parsec.spaces
+  Parsec.char '='
+  Parsec.spaces
+  Parsec.string "??"
+  return $ SynthesisGoal goalId
+
+parseSchemaSkeleton :: Parser RSchema
+parseSchemaSkeleton = do
+  Parsec.char '<'
+  typeVarId <- parseIdentifier
+  Parsec.char '>'
+  fmap (Forall typeVarId) parseSchemaSkeleton
+  <|> fmap Monotype parseType
+  <?> "type schema"
 
 parseType :: Parser (TypeSkeleton Formula)
-parseType = Parsec.choice [parseScalarType, parseFunctionType] <?> "type definition"
+parseType = Parsec.between (Parsec.char '(') (Parsec.char ')') parseType' <|> parseType'
+  where parseType' = Parsec.choice [Parsec.try parseFunctionType, parseScalarType] <?> "type definition"
 
 parseFunctionType :: Parser (TypeSkeleton Formula)
 parseFunctionType = do
-  Parsec.char '('
+  {- Parsec.char '(' -}
   Parsec.spaces
   argId <- parseIdentifier
   Parsec.spaces
@@ -39,7 +157,7 @@ parseFunctionType = do
   Parsec.spaces
   returnType <- parseType
   Parsec.spaces
-  Parsec.char ')'
+  {- Parsec.char ')' -}
   return $ FunctionT argId argType returnType
   <?> "function type"
 
@@ -49,8 +167,7 @@ parseScalarType = Parsec.choice [parseScalarRefType, parseScalarUnrefType] <?> "
     parseScalarUnrefType = do
       baseType <- parseBaseType
       Parsec.spaces
-      typeVarRefinements <- Parsec.many $
-        Parsec.between (Parsec.char '(') (Parsec.char ')') parseType <* Parsec.spaces
+      typeVarRefinements <- Parsec.many $ parseType <* Parsec.spaces
       return $ ScalarT baseType typeVarRefinements ftrue
 
     parseScalarRefType = do
@@ -66,12 +183,16 @@ parseScalarType = Parsec.choice [parseScalarRefType, parseScalarUnrefType] <?> "
 
 parseBaseType :: Parser BaseType
 parseBaseType = Parsec.choice [
-    BoolT <$ Parsec.string "Bool",
-    IntT <$ Parsec.string "Int",
-    fmap DatatypeT parseCustomType,
-    fmap TypeVarT parseIdentifier]
-  where
-    parseCustomType = Applicative.liftA2 (:) Parsec.upper $ Parsec.many (Parsec.alphaNum <|> Parsec.char '_')
+  BoolT <$ Parsec.string "Bool",
+  IntT <$ Parsec.string "Int",
+  fmap DatatypeT parseTypeName,
+  fmap TypeVarT parseIdentifier]
+
+parseSort :: Parser Sort
+parseSort = Parsec.choice [
+  BoolS <$ Parsec.string "Bool",
+  IntS <$ Parsec.string "Int",
+  fmap SetS $ Parsec.string "Set" >> Parsec.spaces >> parseSort]
 
 {-
  - | @Formula@ parsing is broken up into two functions: @parseFormula@ and @parseTerm@. @parseFormula's@ responsible
@@ -85,9 +206,9 @@ parseFormula = Expr.buildExpressionParser exprTable (parseTerm <* Parsec.spaces)
       [unary '!' Not, unary '-' Neg],
       [bin "*" (|*|)],
       [bin "+" (|+|), bin "-" (|-|), bin "/+" (/+/), bin "/*" (/*/), bin "/-" (/-/)],
-      [bin "=" (|=|), bin "/=" (|/=|), bin "<=" (|<=|), bin "<" (|<|), bin ">=" (|>=|), bin ">" (|>|),
+      [bin "==" (|=|), bin "/=" (|/=|), bin "<=" (|<=|), bin "<" (|<|), bin ">=" (|>=|), bin ">" (|>|),
         bin "/<=" (/<=/)],
-      [bin "&" (|&|), bin "|" (|||)],
+      [bin "&&" (|&|), bin "||" (|||)],
       [bin "=>" (|=>|), bin "<=>" (|<=>|)]]
     unary opChar opType = Expr.Prefix (Parsec.char opChar <* Parsec.spaces >> (return $ Unary opType))
     bin opStr func = Expr.Infix parseOpStr Expr.AssocLeft
@@ -119,11 +240,11 @@ parseIntLit = (fmap (IntLit . read) $ Parsec.many1 Parsec.digit) <?> "number"
 
 parseSetLit :: Parser Formula
 parseSetLit = do
-  Parsec.char '{'
+  Parsec.char '['
   Parsec.spaces
   elements <- Parsec.sepBy parseFormula $ Parsec.spaces *> Parsec.char ',' *> Parsec.spaces
   Parsec.spaces
-  Parsec.char '}'
+  Parsec.char ']'
   return $ SetLit UnknownS elements
   <?> "set"
 
@@ -134,7 +255,7 @@ parseMeasure :: Parser Formula
 parseMeasure = do
   measureName <- parseIdentifier
   Parsec.spaces
-  arg <- parseFormula
+  arg <- parseVar
   return $ Measure UnknownS measureName arg
   <?> "measure"
 
@@ -143,3 +264,6 @@ parseIdentifier = Applicative.liftA2 (:) firstChar otherChars <?> "identifier"
   where
     firstChar = Parsec.char '_' <|> Parsec.lower
     otherChars = Parsec.many (Parsec.alphaNum <|> Parsec.char '_')
+
+parseTypeName :: Parser Id
+parseTypeName = (Applicative.liftA2 (:) Parsec.upper $ Parsec.many (Parsec.alphaNum <|> Parsec.char '_')) <?> "type name"
