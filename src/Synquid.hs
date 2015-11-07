749978611f22daf96effcf9d131e71f71a39150f
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2015-11-06 20:57:59 -0500

    Added commend-line arguments for commonly used parameters.

--- src/Synquid.hs
+++ src/Synquid.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable, StandaloneDeriving #-}
 
 module Main where
 
@@ -22,33 +22,56 @@ releaseDate = fromGregorian 2015 11 20
 
 -- | Execute or test a Boogie program, according to command-line arguments
 main = do
-  (CommandLineArgs file) <- cmdArgs cla
-  runOnFile defaultExplorerParams defaultSolverParams file
+  (CommandLineArgs file appMax scrutineeMax fix inc log_) <- cmdArgs cla
+  let explorerParams = defaultExplorerParams { 
+    _eGuessDepth = appMax, 
+    _scrutineeDepth = scrutineeMax, 
+    _fixStrategy = fix, 
+    _incrementalSolving = inc, 
+    _explorerLogLevel = log_ 
+    }
+  let solverParams = defaultSolverParams {
+    solverLogLevel = log_
+    }
+  runOnFile explorerParams solverParams file
   
 {- Command line arguments -}
+
+deriving instance Typeable FixpointStrategy
+deriving instance Data FixpointStrategy
+deriving instance Eq FixpointStrategy
+deriving instance Show FixpointStrategy
 
 data CommandLineArgs
     = CommandLineArgs {
         -- | Input
-        file :: String 
+        file :: String,
+        -- | Explorer params
+        app_max :: Int,
+        scrutinee_max :: Int,
+        fix :: FixpointStrategy,
+        inc :: Bool,
+        log_ :: Int
       }
   deriving (Data, Typeable, Show, Eq)
   
 cla = CommandLineArgs {
-  file        = ""              &= typFile &= argPos 0
+  file            = ""              &= typFile &= argPos 0,
+  app_max         = 3               &= help ("Maximum depth of an application term (default: 3)"),
+  scrutinee_max   = 0               &= help ("Maximum depth of a match scrutinee (default: 0)"),
+  fix             = AllArguments    &= help (unwords ["What should termination metric for fixpoints be derived from?", show AllArguments, show FirstArgument, show DisableFixpoint, "(default:", show AllArguments, ")"]),
+  inc             = True            &= help ("Incremental constraint solving (default: True)"),
+  log_            = 0               &= help ("Logger verboseness level (default: 0)")      
   } &= help "Synthesize goals specified in the input file" &= program programName &= summary (programName ++ " v" ++ versionName ++ ", " ++ showGregorian releaseDate)
 
 -- | Parameters for template exploration
 defaultExplorerParams = ExplorerParams {
   _eGuessDepth = 3,
   _scrutineeDepth = 0,
-  _matchDepth = 1,
+  _matchDepth = 5,
   _condDepth = 1,
   _combineSymbols = PickDepthFirst,
-  -- _combineSymbols = PickInterleave,
   _fixStrategy = AllArguments,
-  -- _fixStrategy = FirstArgument,
-  -- _fixStrategy = DisableFixpoint,
   _polyRecursion = True,
   _incrementalSolving = True,
   _consistencyChecking = False,
@@ -61,23 +84,14 @@ defaultExplorerParams = ExplorerParams {
 -- | Parameters for constraint solving
 defaultSolverParams = SolverParams {
   pruneQuals = True,
-  -- pruneQuals = False,
   optimalValuationsStrategy = MarcoValuations,    
-  -- optimalValuationsStrategy = BFSValuations,    
   semanticPrune = True,
   agressivePrune = True,
-  -- semanticPrune = False,
-  -- agressivePrune = False,    
   candidatePickStrategy = InitializedWeakCandidate,
-  -- candidatePickStrategy = WeakCandidate,
   constraintPickStrategy = SmallSpaceConstraint,
   candDoc = const empty,
   solverLogLevel = 1
-  }
-  
-inequalities = do
-  op <- [Ge, Le, Neq]
-  return $ Binary op (intVar "x") (IntLit 0)
+}
 
 -- | Parse and resolve file, then synthesize the specified goals 
 runOnFile :: ExplorerParams -> SolverParams -> String -> IO ()      
