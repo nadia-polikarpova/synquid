9f825ba736fc453c975417cf30801fd1ad10f6c2
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2016-05-03 17:04:08 -0400

    Bugfix: properly handling abduction of a case that corresponds to an arbitrary constructor

--- src/Synquid/Explorer.hs
+++ src/Synquid/Explorer.hs
@@ -279,15 +279,15 @@ generateMaybeMatchIf env t = (generateOneBranch >>= generateOtherBranches) `mplu
 
     generateMatchesFor env [] pBaseCase t = return pBaseCase
     generateMatchesFor env (matchCond : rest) pBaseCase t = do
-      let matchVar@(Var _ x) = Set.findMin $ varsOf matchCond
+      let (Binary Eq matchVar@(Var _ x) (Cons _ c _)) = matchCond
       let scrT@(ScalarT (DatatypeT scrDT _ _) _) = toMonotype $ symbolsOfArity 0 env Map.! x
       let pScrutinee = Program (PSymbol x) scrT
       let ctors = ((env ^. datatypes) Map.! scrDT) ^. constructors
       let env' = addScrutinee pScrutinee env
-      pBaseCase' <- cut $ inContext (\p -> Program (PMatch pScrutinee [Case (head ctors) [] p]) t) $
+      pBaseCase' <- cut $ inContext (\p -> Program (PMatch pScrutinee [Case c [] p]) t) $
                             generateMatchesFor (addAssumption matchCond env') rest pBaseCase t      
-      pOtherCases <- mapM (cut . generateCase env' matchVar pScrutinee t) (tail ctors)
-      return $ Program (PMatch pScrutinee (Case (head ctors) [] pBaseCase : pOtherCases)) t  
+      pOtherCases <- mapM (cut . generateCase env' matchVar pScrutinee t) (delete c ctors)
+      return $ Program (PMatch pScrutinee (Case c [] pBaseCase : pOtherCases)) t  
 
 
 -- | 'generateE' @env typ@ : explore all elimination terms of type @typ@ in environment @env@
