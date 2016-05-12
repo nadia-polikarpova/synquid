b3d2493e960ec373411c1514c4ff89ba0999bd33
Isaac Grosof <isaacg@mit.edu>
2016-05-12 17:42:45 -0400

    Revert to supertype version. Unsovled performance bug when run on Int-Mult.sq

--- src/Synquid/Explorer.hs
+++ src/Synquid/Explorer.hs
@@ -416,9 +416,9 @@ checkE env typ p@(Program pTerm pTyp) = do
 
               -- Check that pTyp is not a supertype of any prior programs.
               mapM_ (\(op@(Program _ oldTyp), (_, oldEnv)) ->
-                               ifte (areTypesEqual env oldTyp pTyp)
+                               ifte (solveLocally $ Subtype (combineEnv env oldEnv) oldTyp pTyp False)
                                (\_ -> do
-                                    writeLog 1 $ text "Same type as failed predecessor:" <+> pretty pTyp <+> text "with" <+> pretty oldTyp
+                                    writeLog 1 $ text "Supertype as failed predecessor:" <+> pretty pTyp <+> text "with" <+> pretty oldTyp
                                     writeLog 1 $ text "Current program:" <+> pretty p <+> text "Old program:" <+> pretty op
                                     writeLog 1 $ text "Context:" <+> pretty fixedContext
                                     typingState . qualifierMap .= qmap
