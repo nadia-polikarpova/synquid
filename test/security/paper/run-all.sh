489925734bfd2c9d5b198f7ceb7ab07154f98de8
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2016-08-11 15:24:39 -0700

    Introduce lifty mode

--- test/security/paper/run-all.sh
+++ test/security/paper/run-all.sh
@@ -1,4 +1,4 @@
 for fn in [1-6]-*.sq; do
   echo $fn
-  ../../../dist/build/synquid/synquid --print-stats -r $fn Security.sq -l 1 > out/${fn%.sq}.out.txt 2>&1
+  ../../../dist/build/synquid/synquid lifty --print-stats $fn Security.sq -l 1 > out/${fn%.sq}.out.txt 2>&1
 done
