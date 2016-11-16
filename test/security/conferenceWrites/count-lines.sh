b00b59e1a0911563a5d04cb6c0caba555ec8c06a
Shachar Itzhaky <corwin.amber@gmail.com>
2016-11-16 10:42:14 +0200

    Got rid of another stray micro-benchmarks reference, and updated time and line counts.

--- test/security/conferenceWrites/count-lines.sh
+++ test/security/conferenceWrites/count-lines.sh
@@ -1,5 +1,5 @@
-wc -l Conference.sq ../paper/Security.sq ConferenceRepair.sq
+wc -l Conference.sq ../paperWrites/Tagged.sq ../paperWrites/Prelude.sq ConferenceRepair.sq
 echo
 wc -l ConferenceImpl.hs ConferenceMain.hs
 echo
-wc -l Conference.sq ../paper/Security.sq ConferenceRepair.sq ConferenceImpl.hs ConferenceMain.hs
+wc -l Conference.sq ../paperWrites/Tagged.sq ../paperWrites/Prelude.sq ConferenceRepair.sq ConferenceImpl.hs ConferenceMain.hs
