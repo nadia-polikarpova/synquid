b994f8ffcb453abb784f867bc7aec9b4d8400f9b
Travis Hance <tjhance7@gmail.com>
2018-03-14 16:29:38 -0400

    get rid of this .preprocessed file

--- test/security/icfp/gradr/run-all.sh
+++ test/security/icfp/gradr/run-all.sh
@@ -1,4 +1,3 @@
 mkdir -p out
-synquid lifty --print-stats --file=gradr.sq \
-    --libs=../Prelude.sq --libs=../Tagged.sq --libs=models.sq.preprocessed \
+synquid lifty --print-stats --file=gradr.sq --libs=../Prelude.sq --libs=../Tagged.sq --libs=models.sq \
     > out/gradr.out.txt 2>&1
