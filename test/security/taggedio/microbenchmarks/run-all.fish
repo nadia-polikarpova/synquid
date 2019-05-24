d99fe3adf885a54b9db7a74fe0a5adbb8976afb4
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2019-05-23 17:35:59 -0700

    Repair works for 05

--- test/security/taggedio/microbenchmarks/run-all.fish
+++ test/security/taggedio/microbenchmarks/run-all.fish
@@ -1,5 +1,5 @@
 for fn in *.sq
-    if test "$fn" != "05-StateUpdate.sq"
+    if test "$fn" != "11-Instagram.sq"
       echo $fn
       synquid lifty --print-stats --file=$fn --libs=../Prelude.sq --libs=../Tagged.sq > out/$fn.out 2>&1
     end
