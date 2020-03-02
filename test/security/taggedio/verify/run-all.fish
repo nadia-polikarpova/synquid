a57d30af86b653f9130387c61955d77abc455333
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2020-03-02 10:25:18 -0800

    Update benchmarks

--- test/security/taggedio/verify/run-all.fish
+++ test/security/taggedio/verify/run-all.fish
@@ -4,7 +4,7 @@ synquid lifty --print-stats --verify --file=../Prelude.sq > out/Prelude.out 2>&1
 echo Tagged
 synquid lifty --print-stats --verify --file=../Tagged.sq --libs=../Prelude.sq > out/Tagged.out 2>&1
 
-for fn in good good1 good2 good3 bad1 bad2
+for fn in ok1 ok2 ok3 ok4 bad1 bad2 bad3 bad4
     echo $fn
     synquid lifty --print-stats --verify --only=$fn --file=Test.sq --libs=../Prelude.sq --libs=../Tagged.sq > out/$fn.out 2>&1
   end  
