db5f6a4334584495b8baea05f2577f55ea538cc4
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2020-03-03 14:28:08 -0800

    Update health manager, merge tables, fish scripts

--- test/security/taggedio/conference/count-lines-nadia.sh
+++
@@ -1,5 +0,0 @@
-wc -l Conference.sq ../Tagged.sq ../Prelude.sq ConferenceRepair.sq
-echo
-wc -l ConferenceImpl.hs ConferenceMain.hs
-echo
-wc -l Conference.sq ../Tagged.sq ../Prelude.sq ConferenceRepair.sq ConferenceImpl.hs ConferenceMain.hs
---
+++ test/security/taggedio/health/run-all.fish
@@ -0,0 +1,1 @@
+synquid lifty --libs=../Prelude.sq --libs=../Tagged.sq --file=HealthWeb.sq --print-stats > out/HealthWeb.sq.out 2>&1
