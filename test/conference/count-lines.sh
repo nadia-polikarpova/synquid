853d53a25b56777ad6b3d065f5b6b7cffe442309
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2020-05-21 11:34:36 -0700

    Final cleanup

--- test/conference/count-lines.sh
+++
@@ -1,5 +0,0 @@
-wc -l Conference.sq ../paperWrites/Tagged.sq ../paperWrites/Prelude.sq ConferenceRepair.sq
-echo
-wc -l ConferenceImpl.hs ConferenceMain.hs
-echo
-wc -l Conference.sq ../paperWrites/Tagged.sq ../paperWrites/Prelude.sq ConferenceRepair.sq ConferenceImpl.hs ConferenceMain.hs
